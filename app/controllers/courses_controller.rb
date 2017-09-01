92c6393a38fb27b9fc9c07fc6c12b37e1cea3fcd
Faris Parkar <farisparkar@gmail.com>
2017-09-01 06:25:35 +0000

    course/attendance update

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,7 +4,25 @@ class CoursesController < ApplicationController
    end
    
    def new
-       
+       @course = Course.new
    end
+   
+   def show
+       @course = Course.find(params[:id])
+   end
+   
+   def create
+        @course = Course.new(course_params)
+        if @course.save
+            redirect_to @course
+        else
+            flash[:danger] = @course.errors.full_messages.to_sentence
+            render 'new'
+        end
+    end
     
+     private
+        def course_params
+            params.require(:course).permit(:name, :description, :coursecode)
+        end
 end

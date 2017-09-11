7a5eee89d2a0a94a04e68ce6a153dab241ecfff4
Faris Parkar <farisparkar@gmail.com>
2017-09-11 22:41:31 +0000

    front page cleaning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,9 @@ class ApplicationController < ActionController::Base
     before_action :add_parameters_to_user, if: :devise_controller?
   
   def add_parameters_to_user
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name]) 
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
-   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name])
-   devise_parameter_sanitizer.permit(:account_update, keys: [:last_name])
-    
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:username]) 
+   devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+   
   end
   
 end
